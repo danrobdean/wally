bb1b64fec7f081f76e05ebdd143c4367d494b647
Dean <danieldean@improbable.io>
2019-09-06 15:45:42 +0100

    Added highlighting, formatting, give-up button.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A website for sticking our heads on a where's Wally for some reason.
 
+The website can be accessed at [http://danrobdean.github.io/wally](http://danrobdean.github.io/wally).
+
 Run the server with `./run.sh` in git bash on windows or terminal for mac/linux.
 
 ![wally](wally.jpg "Where is he?")
--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-names = {
-    spooky: {
-        src: "head.jpg",
-        text: "I am a wally"
-    },
-    cheery: {
-        src: "head2.jpg",
-        text: "I am also a wally"
+class Profile {
+    constructor(src, name, role, fact) {
+        this.src = src;
+        this.name = name;
+        this.role = role;
+        this.fact = fact;
     }
+}
+
+names = {
+    spooky: new Profile("head.jpg", "Wally", "Graduate Wally Engineer", "I love where's wally!"),
+    cheery: new Profile("head2.jpg", "Waldo", "Sophomore Waldo Wrangler", "I love me a good waldo yeehaw")
 };
 
 function setHeads() {
@@ -19,10 +22,20 @@ function setHeads() {
 
 function clickHead(name) {
     document.getElementById("modal").classList.add("visible");
+    document.getElementById(name).classList.add("highlighted");
     document.getElementById("bigHead").src = names[name].src;
-    document.getElementById("headText").textContent = names[name].text;
+    document.getElementById("name").textContent = names[name].name;
+    document.getElementById("role").textContent = names[name].role;
+    document.getElementById("fact").textContent = `\"` + names[name].fact + `\"`;
 }
 
 function hideModal() {
     document.getElementById("modal").classList.remove("visible");
+}
+
+function highlightAll() {
+    var heads = document.getElementsByClassName("head");
+    for (let head of heads) {
+        head.classList.add("highlighted");
+    }
 }
