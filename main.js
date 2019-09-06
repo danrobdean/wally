27883f42ca80d51ba1d9af9fc8a19bee41bf4d76
Dean <danieldean@improbable.io>
2019-09-06 16:35:56 +0100

    Final fixed modal on mobile.

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 class Profile {
-    constructor(src, name, role, fact) {
+    constructor(src, name, role, fact, left, top) {
         this.src = src;
         this.name = name;
         this.role = role;
         this.fact = fact;
+        this.left = left;
+        this.top = top;
     }
 }
 
 names = {
-    spooky: new Profile("head.jpg", "Wally", "Graduate Wally Engineer", "I love where's wally!"),
-    cheery: new Profile("head2.jpg", "Waldo", "Sophomore Waldo Wrangler", "I love me a good waldo yeehaw")
+    spooky: new Profile("head.jpg", "Wally", "Graduate Wally Engineer", "I love where's wally!", "303px", "325px"),
+    cheery: new Profile("head2.jpg", "Waldo", "Sophomore Waldo Wrangler", "I love me a good waldo yeehaw", "409px", "657px")
 };
 
 function setHeads() {
@@ -27,6 +29,10 @@ function clickHead(name) {
     document.getElementById("name").textContent = names[name].name;
     document.getElementById("role").textContent = names[name].role;
     document.getElementById("fact").textContent = `\"` + names[name].fact + `\"`;
+
+    // var modalInner = document.getElementById("modalInner");
+    // modalInner.style.left = names[name].left;
+    // modalInner.style.top = names[name].top;
 }
 
 function hideModal() {
