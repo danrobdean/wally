13e5956b39b3217ad2723d6019bedb878b65bd57
Dean <danieldean@improbable.io>
2019-09-09 15:47:06 +0100

    Added fun fact for paul

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ names = {
     michael: new Profile("pics/michael.jpg", "Michael Gillan", "Graduate Software Engineer", "I have never had fun in my life.", "0px", "0px"),
     miron: new Profile("pics/miron.jpg", "Miron Zelina", "Graduate Software Engineer", "I once ended up in St Mary's hospital because I lost my water bottle and forgot to drink for two days!", "0px", "0px"),
     nikita: new Profile("pics/nikita.jpg", "Nikita Hukerikar", "Graduate Software Engineer", "Talk to me about the year I lived in Hong Kong!", "0px", "0px"),
-    paul: new Profile("pics/paul.jpg", "Paul Larsen", "Graduate Software Engineer", "I am too boring to think of a fun fact!", "0px", "0px"),
+    paul: new Profile("pics/paul.jpg", "Paul Larsen", "Graduate Software Engineer", "I recently decided it was about time i got a drivers license, but ended up getting a divers license instead.", "0px", "0px"),
     teri: new Profile("pics/teri.jpg", "Teri Drummond", "Partner Engineering - Games Solutions", "I love video games - currently all my spare time is going into the new Fire Emblem. I'm joining Improbable from a games programming background, and I'm excited to work on innovative tech that will help a wide range of game developers!", "0px", "0px"),
     tilman: new Profile("pics/tilman.jpg", "Tilman Schmidt", "Graduate Software Engineer", "I can do 4-ball juggling! For a little bit anyway.", "0px", "0px"),
     vlad: new Profile("pics/vlad.jpg", "Vlad Barvinko", "Graduate Software Engineer", "In true Dutch fashion, I live on my bike. Tell me about your favourite type of transport!", "0px", "0px")
